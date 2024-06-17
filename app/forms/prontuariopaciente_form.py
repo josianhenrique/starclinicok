b316491a512425f5652f8e3815f3add351b029be
Josian Henrique <josian.henrique8165@gmail.com>
2024-06-17 16:10:31 -0300

    salvando

--- app/forms/prontuariopaciente_form.py
+++ app/forms/prontuariopaciente_form.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
+from wtforms import TextAreaField
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class ProntuarioPacienteForm(FlaskForm):
-    historico = StringField("Histórico", validators=[DataRequired()])
+    historico = TextAreaField("Histórico", validators=[DataRequired()])
     nome = StringField("Nome", validators=[DataRequired()])
     whatsapp = StringField("WhatsApp", validators=[DataRequired()])
     email = StringField("E-mail", validators=[DataRequired()])
