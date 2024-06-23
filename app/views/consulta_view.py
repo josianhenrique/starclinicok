4a90d9a80680bc9d25770d28f76a544680d6d321
Josian Henrique <josian.henrique8165@gmail.com>
2024-06-22 23:43:44 -0300

    vitoria

--- app/views/consulta_view.py
+++ app/views/consulta_view.py
@@ -8,7 +8,6 @@ from app.models import prontuariopaciente_model, recepcionista_model, medico_mod
 def cadastrar_consulta():
     form = ConsultaForm()
 
-    
     prontuariospacientes = prontuariopaciente_model.ProntuarioPaciente.query.all()
     prontuario_escolhas = [(prontuario.id, prontuario.nome) for prontuario in prontuariospacientes]
     form.fk_prontuario_paciente.choices = prontuario_escolhas
@@ -48,14 +47,18 @@ def cadastrar_consulta():
             db.session.add(consulta)
             db.session.commit()
             flash("Consulta cadastrada com sucesso!", "success")
-            return redirect(url_for('cadastrar_consulta'))
+            return redirect(url_for('ver_consultas'))
         except Exception as e:
             db.session.rollback()
             flash(f"Erro ao cadastrar consulta: {e}", "danger")
 
     return render_template("consulta/consulta.html", form=form)
 
-
+@app.route('/consultas/<int:id>')
+def ver_uma_consulta(id):
+    consulta = Consulta.query.get_or_404(id)
+    return render_template('consulta/verumaconsulta.html', consulta=consulta)
+   
 @app.route("/removerconsulta/<int:id>", methods=["GET", "POST"])
 def remover_consulta(id):
     consulta_remover = Consulta.query.get_or_404(id)
@@ -65,8 +68,54 @@ def remover_consulta(id):
         db.session.commit()
         flash("Consulta removida com sucesso!", "success")
     except Exception as e:
-        print("Erro ao remover consulta:", e)
         db.session.rollback()
-        flash("Erro ao remover consulta. Por favor, tente novamente mais tarde.", "error")
+        flash(f"Erro ao remover consulta: {e}", "danger")
 
     return redirect(url_for('ver_consultas'))
+
+@app.route("/verconsultas")
+def ver_consultas():
+    consultas = Consulta.query.all()
+    return render_template("consulta/verconsultas.html", consultas=consultas)
+
+@app.route("/editarconsulta/<int:id>", methods=["GET", "POST"])
+def editar_consulta(id):
+    consulta_editar = Consulta.query.get_or_404(id)
+    form = ConsultaForm(obj=consulta_editar)
+
+    prontuariospacientes = prontuariopaciente_model.ProntuarioPaciente.query.all()
+    prontuario_escolhas = [(prontuario.id, prontuario.nome) for prontuario in prontuariospacientes]
+    form.fk_prontuario_paciente.choices = prontuario_escolhas
+
+    planos_saude = planosaude_model.PlanoSaude.query.all()
+    plano_escolhas = [(plano.id, plano.nome) for plano in planos_saude]
+    form.fk_plano_de_saude_id.choices = plano_escolhas
+    
+    medicos = medico_model.Medico.query.all()
+    medico_escolhas = [(medico.id, medico.nome) for medico in medicos]
+    form.fk_medico_id.choices = medico_escolhas
+    
+    recepcionistas = recepcionista_model.Recepcionista.query.all()
+    recepcionista_escolhas = [(recepcionista.id, recepcionista.nome) for recepcionista in recepcionistas]
+    form.fk_recepcionista_id.choices = recepcionista_escolhas
+
+    if form.validate_on_submit():
+        consulta_editar.valor = form.valor.data
+        consulta_editar.data = form.data.data
+        consulta_editar.horario = form.horario.data
+        consulta_editar.recepcionista_id = form.recepcionista_id.data
+        consulta_editar.medico_id = form.medico_id.data
+        consulta_editar.prontuario_paciente_id = form.prontuario_paciente_id.data
+        consulta_editar.plano_de_saude_id = form.plano_de_saude_id.data
+        form.populate_obj(consulta_editar)
+
+        try:
+            db.session.commit()
+            flash("Consulta atualizada com sucesso!", "success")
+            return redirect(url_for('ver_consultas'))
+        except Exception as e:
+            print("Erro ao atualizar consulta:", e)
+            db.session.rollback()
+            flash("Erro ao atualizar consulta. Por favor, tente novamente mais tarde.", "danger")
+
+    return render_template("consulta/consulta.html", form=form, editar=True, consulta_editar=consulta_editar)
